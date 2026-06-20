56aa7128217a6400ea2e484618033433bb597491
karisnguyenn <uwumoaw1101@gmail.com>
2026-06-20 18:05:38 +0700

    update

--- manager/settings.gradle.kts
+++ manager/settings.gradle.kts
@@ -6,14 +6,16 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
